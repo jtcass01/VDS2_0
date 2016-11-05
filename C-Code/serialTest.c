1173fb4c305dbdd2537d2bd89af20ae483a84e3d
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-11-05 15:01:03 -0400

    Update serialTest.c

--- C-Code/serialTest.c
+++ C-Code/serialTest.c
@@ -1,57 +1,30 @@
- /*
-  Communications Matlab <--> Arduino
-  Arduino file 1 for use with Matlab file 1 
-  L. Toms
-  establishContact() routine by:
-  by Tom Igoe and Scott Fitzgerald
-  http://www.arduino.cc/en/Tutorial/SerialCallResponse
-  other ideas from
-  robot grrrl
-  http://robotgrrl.com/blog/2010/01/15/arduino-to-matlab-read-in-sensor-data/
-  */
-   int ledPin=13;
-   int i = 1;
-   char response = '\0';
+ char response = '\0';
    
-   void setup() {
+void setup() {
      
    // start serial port at 9600 bps:
    Serial.begin(9600);
    
-   establishContact();  // send a byte to establish contact until receiver responds
-   Serial.print("Test");
-   Serial.flush();
- }
+   handShake();  // send a byte to establish contact until receiver responds
+}
 
  void loop() {
-   if(Serial.available() >= 4){
-     response = Serial.read();
+  if(Serial.available() >= 4){
+    response = Serial.read();
      
-     if(response == 'B'){
-       Serial.print("B RECIEVED");
-       Serial.flush();
-     }
-     
-     if(response == 'E'){
-       Serial.print("E RECIEVED");
-       Serial.flush();     
-     }
-     
-     /*Serial.write(response);
-     Serial.flush();
-     response = response + 1;
-     Serial.write(response);
-     Serial.flush();
-     delay(1000);*/
-   }
-   
-   /*
-   Serial.println(i);
-   i=i+1;  
-   delay(1000);*/
+    switch(response){
+    case 'B':
+      Serial.print("B RECIEVED");
+      Serial.flush();
+      break;
+    case 'E':
+      Serial.print("E RECIEVED");
+      Serial.flush();     
+    }
+  }
  }
 
- void establishContact() {
+ void handShake() {
      while (Serial.available() <= 0) {
        Serial.write('A');   // send a capital A
        delay(300);
