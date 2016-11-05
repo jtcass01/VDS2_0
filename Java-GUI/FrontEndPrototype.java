1708c8ee920047289b90df5f76ee39a18827ff87
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-11-05 15:01:58 -0400

    Update FrontEndPrototype.java

--- Java-GUI/FrontEndPrototype.java
+++ Java-GUI/FrontEndPrototype.java
@@ -1,36 +1,68 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class FrontEndPrototype {
 
 	public static void main(String[] args){
 		BackendDesign backend = new BackendDesign();
+		Scanner scan = new Scanner(System.in);
+		int nextFunction = 0;
+		boolean cont = true;
+		boolean validResponse = false;
 		
 		while(!backend.isConnected()){
 			System.out.println("YOLO");
 			try {
 				Thread.sleep(3000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 		}
-		System.out.println("Writing B");
-		backend.wData('B');
-		backend.wData('B');
-		backend.wData('B');
-		backend.wData('B');
-		backend.wData('B');
-/*		System.out.println("Writing C");
-		backend.wData('C');
-		System.out.println("Writing D");
-		backend.wData('D');
-		System.out.println("Writing E");
-		backend.wData('E');
-		System.out.println("Writing F");
-		backend.wData('F');
-		System.out.println("Writing G");
-		backend.wData('G');
-		System.out.println("Writing H");
-		backend.wData('H');*/
+		
+		while(cont){
+			while(!validResponse){
+				printMenu();
+				try{
+					nextFunction = scan.nextInt();
+					validResponse = true;
+				} catch (InputMismatchException e){
+					System.out.println("You did not input an integer.  Please try again.");
+				}
+			}
+			validResponse = false;
+			
+			switch(nextFunction){
+			case 1:
+				System.out.println("Writing B");
+				backend.wData('B');
+				break;
+			case 2:
+				System.out.println("Writing E");
+				backend.wData('E');
+				break;
+			case 3:
+				backend.disconnect();
+				cont = false;
+				System.out.println("\nGoodbye : )");
+				break;
+			default:
+				System.out.println("Integer response is not located on menu.  Please try again.");
+				break;
+			}
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void printMenu(){
+		System.out.println("==========MENU==========");
+		System.out.println("1 ) write B");
+		System.out.println("2 ) write E");
+		System.out.println("3 ) disconnect");		
+		System.out.print("Enter selction: ");
 	}
 }
