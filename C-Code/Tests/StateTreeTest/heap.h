2ac3d1912dcdbab8d496b6b5c217d28985b3ef6e
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-12-08 12:57:14 -0500

    Update heap.h

--- C-Code/Tests/StateTreeTest/heap.h
+++ C-Code/Tests/StateTreeTest/heap.h
@@ -8,29 +8,59 @@
 #define CEILING(X) ( ((X) > 0) ? CEILING_POS(X) : CEILING_NEG(X) )
 
 short hasLeft(int, int, int*);
-short hasRight(int, int, int*);
 int getLeft(int, int, int*);
+short hasRight(int, int, int*);
 int getRight(int, int, int*);
 short isNew(int*, int);
 
 
-int* getBinaryHeap(int capacity){
-  int heap[capacity];
-  int mid = capacity / 2, i = 1;
+void getBinaryHeap(int* heap, int capacity){
+  int mid = capacity / 2, i = 1, j=0;
 
   heap[0] = mid;
 
-  if(hasLeft(mid,0,heap)){
-    heap[i] = getLeft(mid,0,heap);
-    i++;
-  }
-  if(hasRight(mid,capacity,heap)){
-    i++;
+  for(;i<capacity;){
+    printf("\n\nNode being tested: %i     j = %i", heap[j], j);
+    if(heap[j] < mid){
+      if(hasLeft(heap[j],0,heap)){
+        printf("\nInserting %i at leftNode.     i = %i     j = %i     heapLocation = %i", getLeft(heap[j],0,heap), i, j, (2*j+1));
+        heap[(2*j+1)] = getLeft(heap[j],0,heap);
+        i++;
+      }
+      if(hasRight(heap[j],mid,heap)){
+        printf("\nInserting %i at rightNode.     i = %i     j = %i     heapLocation = %i", getRight(heap[j],mid,heap), i, j, (2*j+2));
+        heap[(2*j+2)] = getRight(heap[j],mid,heap);
+        i++;
+      }
+    } else if (heap[j] > mid){
+      if(hasLeft(heap[j],mid,heap)){
+        printf("\nInserting %i at leftNode.     i = %i     j = %i     heapLocation = %i", getLeft(heap[j],mid,heap), i, j, (2*j+1));
+        heap[(2*j+1)] = getLeft(heap[j],mid,heap);
+        i++;
+      }
+      if(hasRight(heap[j],capacity,heap)){
+        printf("\nInserting %i at rightNode.     i = %i     j = %i     heapLocation = %i", getRight(heap[j],capacity,heap), i, j, (2*j+2));
+        heap[(2*j+2)] = getRight(heap[j],capacity,heap);
+        i++;
+      }
+    } else {
+      if(hasLeft(mid,0,heap)){
+        heap[i] = getLeft(mid,0,heap);
+        i++;
+      }
+      if(hasRight(mid,capacity,heap)){
+        heap[i] = getRight(mid, capacity, heap);
+        i++;
+      }
+    }
+    j++;
   }
 
-  for(;i<capacity;){
-    i++;
+/*  if(heap[capacity-1] == 0){
+    heap[capacity-1] = capacity;
   }
+
+  heap[capacity] = 0;*/
 }
 
 short hasLeft(int current, int min, int* heap){
@@ -38,18 +68,27 @@ short hasLeft(int current, int min, int* heap){
 
   possibility = (int)((current - min) / 2);
   possibility = current - possibility;
-  printf("\npossibility %i\n", possibility);
 
   if(possibility == 0){
     return 0;
   } else {
     if(isNew(heap,possibility)){
-      printf("\nLeft node possibilty confirmed.\n");
+      printf("\nLeft node possibilty confirmed! New left node = %i", possibility);
       return 1;
     } else {
+      printf("\nLeft node possibilty denied! Attempted left node = %i", possibility);
       return 0;
     }
   }
+}
+
+int getLeft(int current, int min, int* heap){
+  int possibility = 0;
+
+  possibility = (int)((current - min) / 2);
+  possibility = current - possibility;
+
+  return possibility;
 }
 
 
@@ -58,22 +97,48 @@ short hasRight(int current, int max, int* heap){
   int possibility = 0;
 
   temp = ((double)max-(double)current)/2;
-  printf("\nTEMP %f\n", temp);
+//  printf("\nTEMP %f\n", temp);
 
   possibility = (int)CEILING(temp);
   possibility += current;
-  printf("\npossibility %i\n", possibility);
+//  printf("\npossibility %i\n", possibility);
 
-  if(possibility == 0){
-    return 0;
-  } else {
-    if(isNew(heap,possibility)){
-      printf("\nRight node possibilty confirmed.\n");
+  if(current == (max-1)){
+    if(isNew(heap,max)){
       return 1;
     } else {
       return 0;
     }
   }
+
+  if(possibility == 0){
+    return 0;
+  } else {
+    if(isNew(heap,possibility)){
+      printf("\nRight node possibilty confirmed! New right node = %i", possibility);
+      return 1;
+    } else {
+      printf("\nRight node possibilty denied! Attempted right node = %i", possibility);
+      return 0;
+    }
+  }
+}
+
+int getRight(int current, int max, int* heap){
+  double temp = 0;
+  int possibility = 0;
+
+  temp = ((double)max-(double)current)/2;
+  possibility = (int)CEILING(temp);
+  possibility += current;
+
+  if(current == max-1){
+    if(isNew(heap,max)){
+      return max;
+    }
+  }
+
+  return possibility;
 }
 
 short isNew(int* heap, int possibility){
@@ -87,8 +152,8 @@ short isNew(int* heap, int possibility){
 
 void printHeap(int* heap, short size){
   printf("\n\n----- PRINTING HEAP ------\n");
-  for(unsigned short i = 0; i < size; i++){
-    printf("\ni = %i     heap[i] = %i", i, heap[i]);
+  for(unsigned short i = 0; heap[i] != 0; i++){
+    printf("\ni = %i       heap[i] = %i", i, heap[i]);
   }
 }
 
