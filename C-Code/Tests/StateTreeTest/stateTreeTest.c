80276a890c8d24a8d1d334c9730dabf9f0380cc8
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-12-07 23:52:52 -0500

    Create stateTreeTest.c

---
+++ C-Code/Tests/StateTreeTest/stateTreeTest.c
@@ -0,0 +1,15 @@
+#include "state.h"
+#include "stateTree.h"
+
+#define CAPACITY (16)
+
+int heap[CAPACITY];
+
+void setup() {
+  //zeroOutHeap(heap, capacity);
+}
+
+void loop() {
+  //Serial.println(heap[0]);  
+  Serial.println(CAPACITY);
+}
