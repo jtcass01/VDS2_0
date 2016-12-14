af1817198b405743231f9bb0fdc53ecc32dc3bde
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-12-13 20:36:37 -0500

    Update stateTree.h

--- C-Code/Tests/StateTreeTest/stateTree.h
+++ C-Code/Tests/StateTreeTest/stateTree.h
@@ -8,32 +8,70 @@ struct stateTree {
   unsigned short capacity;
 };
 
+struct stateTree* createStateTree(int);
+void deleteStateTree(struct stateTree*);
+void initializeTree(struct stateTree*);
+void insert(const int, struct stateTree*);
+void insert(const int, struct state*);
+void printTree(struct stateTree*);
+void printTree(struct state*);
 
-struct stateTree* createStateTree(int cap){
-  struct stateTree* pStateTree = malloc(sizeof(short) + (cap*sizeof(struct state)));
+
+struct stateTree* createStateTree(int capacity){
+  struct stateTree* pStateTree = malloc(sizeof(short) + (capacity*sizeof(struct state)));
   if(pStateTree != NULL){
-    pStateTree->capacity = cap;
+    pStateTree->capacity = capacity;
 //    initializeTree(pStateTree);
   } else {
     free(pStateTree);
   }
 }
 
+
 void deleteStateTree(struct stateTree* pStateTree){
   deleteState(pStateTree->root);
   free(pStateTree);
 }
 
-/*
+
 void initializeTree(struct stateTree* pStateTree){
-  int mid = 0, max = 0, i = 1, left = 0, right = 0;
-  int order[pStateTree->capacity];
+  int binaryHeap[pStateTree->capacity];
 
-  mid = (pStateTree->capacity/2);
-  order[0] = mid;
-  printf("\nmid = %i\n",mid);
-//  if(hasLeft(mid,0){
+  getBinaryHeap(binaryHeap, pStateTree->capacity);
 
-//  }
+  for(unsigned short binaryHeap_i = 0; binaryHeap_i < pStateTree->capacity; binaryHeap_i++){
+    insert(binaryHeap[binaryHeap_i], pStateTree);
+  }
 }
-*/
+
+
+void insert(const int pos, struct stateTree* pStateTree){
+  insert(pos, pStateTree->root);
+}
+
+void insert(const int pos, struct state* pState){
+  if(pState == NULL){
+    pState = createState(pos);
+  } else if (pos < pState->position){
+    insert(pos, pState->leftChild);
+  } else if (pos > pState->position){
+    insert(pos, pState->rightChild);
+  }
+}
+
+
+int isEmpty(struct stateTree* pStateTree){
+  return ((pStateTree->root == NULL) ? 1 : 0);
+}
+
+
+void printTree(struct stateTree* pStateTree){
+  printf("\n\nPRINTING BINARY TREE");
+
+
+}
+
+
+void printTree(struct state* pState){
+
+}
