aeed335aac228d9626e6a979967cf7cb90eb99c7
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-12-14 15:49:13 -0500

    Update state.h

--- C-Code/Tests/StateTreeTest/state.h
+++ C-Code/Tests/StateTreeTest/state.h
@@ -2,6 +2,7 @@
 #define __STATE_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 struct state {
   unsigned short position;
@@ -14,14 +15,22 @@ struct state {
 };
 
 
+struct state* createState(int);
+void deleteState(struct state*);
+void printState(struct state*);
+
 struct state* createState(int pos){
   struct state* pState = malloc(sizeof(struct state));
+  printf("\n\n-----Creating state with position value: %i-----",pos);
   if(pState != NULL){
     pState->position = pos;
+    pState->time = 0;
     pState->alt = 0;
     pState->vel = 0;
     pState->accel = 0;
+    printState(pState);
   } else {
+    printf("\n\nERROR WITHIN CREATE STATE!! NULL POINTER");
     free(pState);
   }
   return pState;
@@ -29,6 +38,17 @@ struct state* createState(int pos){
 
 void deleteState(struct state* pState){
   free(pState);
+}
+
+void printState(struct state* pState){
+  printf("\n\n----- Printing State -----");
+  printf("\nPosition = %i", pState->position);
+  printf("\nTime = %i", pState->time);
+  printf("\nAltitude = %i", pState->alt);
+  printf("\nVelocity = %i", pState->vel);
+  printf("\nAcceleration = %i", pState->accel);
+  printf("\nleftChild = %p", pState->leftChild);
+  printf("\nrightChilde = %p", pState->rightChild);
 }
 
 
