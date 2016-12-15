dbd483fcb1e9f93cd26264989ea68ee86e8c7594
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-12-15 10:52:31 -0500

    Update stateTreeTest.c

--- C-Code/Tests/StateTreeTest/stateTreeTest.c
+++ C-Code/Tests/StateTreeTest/stateTreeTest.c
@@ -8,48 +8,33 @@
 int heap[CAPACITY];
 struct stateTree* pRawStates;
 struct state* pTestState;
+int testInt;
 void setup();
 
 int main(void){
   setup();
-/*  printf("\n----- pTestState Created -----\n");
-  printf("\nPosition = %i", pTestState->position);
-  printf("\nTime = %f", pTestState->time);
-  printf("\nAltitude = %f", pTestState->alt);
-  printf("\nVelocity = %f", pTestState->vel);
-  printf("\nAcceleration = %f", pTestState->accel);
-  printf("\nleftChild = %p", pTestState->leftChild);
-  printf("\nrightChild = %p", pTestState->rightChild);
+  pTestState->time = 100;
+  pTestState->alt = 200;
+  pTestState->vel = 300;
+  pTestState->accel = 400;
 
-  printf("\n----- pRawStates Created -----\n");
-  printf("\nCapacity = %i", pRawStates->capacity);
 
-  zeroOutHeap(heap, CAPACITY);
-  getBinaryHeap(heap, CAPACITY);
-*/
-  /*if(isNew(heap,1)){
-    printf("\n1 is new.");
-  } else {
-    printf("\n1 is not new.");
-  }*/
-  //hasRight(heap, heap[0], CAPACITY);
-  //hasLeft(heap, heap[0], 0);
-//  printHeap(heap);
+  testInt = getTime(pRawStates, 1);
+
+
+
+  setState(pRawStates, 8, pTestState);
+
+  printTree(pRawStates);
+
+  deleteStateTree(pRawStates);
+  printf("\n\n----- pRawStates Deleted -----\n");
+
+  printf("testInt = %i", testInt);
 
 
   deleteState(pTestState);
   printf("\n\n----- pTestState Deleted -----\n");
-
-/*  printf("\nPosition = %i", pRawStates->root->position);
-  printf("\nTime = %f", pRawStates->root->time);
-  printf("\nAltitude = %f", pRawStates->root->alt);
-  printf("\nVelocity = %f", pRawStates->root->vel);
-  printf("\nAcceleration = %f", pRawStates->root->accel);
-  printf("\nleftChild = %p", pRawStates->root->leftChild);
-  printf("\nrightChild = %p", pRawStates->root->rightChild);*/
-
-  deleteStateTree(pRawStates);
-  printf("\n\n----- pRawStates Deleted -----\n");
 
   return 1;
 }
@@ -57,8 +42,6 @@ int main(void){
 void setup() {
   pTestState = createState(5);
   pRawStates = createStateTree(CAPACITY);
-
-  //rawStates = createStateTree(CAPACITY);
 }
 
 void loop() {
